f284e5b481f87bfbae806e6d767c2744b804e106
Tocka <boubacar.koita@gmail.com>
2016-06-17 03:28:06 +0200

    update tiles tempating configuration

---
+++ src/main/java/org/theblog/controllers/IndexController.java
@@ -0,0 +1,16 @@
+package org.theblog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class IndexController {
+	
+	@RequestMapping("/index")
+    public String index() {
+		return "index";
+    }
+		
+	
+}
